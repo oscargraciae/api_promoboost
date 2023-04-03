387d2120540e6a61b8a6990a7ceb07b690fc1b7d
Oscar Gracia <oscar.graciae@gmail.com>
2023-04-03 14:46:42 -0600

    Se cambio el puerto a 9001

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ if (global.redis !== undefined) {
 app.use(logErrors)
 app.use(errorHandler)
 
-const port = 8000
+const port = 9001
 
 app.listen(port, () => { console.log(`Server listen on port: ${port}`) })
 
